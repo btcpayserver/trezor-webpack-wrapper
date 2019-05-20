a650e5afba410450b4bbfa20cd15455c2bb5ae9a
Kukks <evilkukka@gmail.com>
2019-05-20 07:20:57 +0200

    add trezor wrapper

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import Cat from './cat.js';
-import Dog from './dog.js';
-export { Dog, Cat };
+var trezor = require('trezor.js')
+var trezorLink = require('trezor-link')
+
+import trezorConfig from "./config";
+
+window.trezor = trezor;
+window.trezorLink = trezorLink;
+window.trezorConfig = trezorConfig;
+
+export default {
+    ...trezor,
+    trezorConfig
+};
+
